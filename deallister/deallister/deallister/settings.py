0f099c0fa8c9f58fd8f3977970cffe75790b9b03
aravindvj <a.venugopalan@ed.ac.uk>
2018-11-14 01:10:58 +0000

    Added model

--- deallister/deallister/deallister/settings.py
+++ deallister/deallister/deallister/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dealpages.apps.DealpagesConfig'
+    'dealpages.apps.DealpagesConfig',
 ]
 
 MIDDLEWARE = [
--- deallister/deallister/dealpages/models.py
+++ deallister/deallister/dealpages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class item(models.Model):
+    item = models.CharField(max_length=20)
+    price = models.IntegerField()
+    origin = models.URLField()
+    description = models.TextField()
+
+
+    def __str__(self):
+        return self.item
