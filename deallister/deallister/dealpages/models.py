27de91e1cfc4a9aa3ce663d1fe82d702e33961dd
aravindvj <a.venugopalan@ed.ac.uk>
2018-11-14 04:33:58 +0000

    Change

--- deallister/deallister/dealpages/models.py
+++ deallister/deallister/dealpages/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class item(models.Model):
-    item = models.CharField(max_length=20)
+class Item(models.Model):
+    item_name = models.CharField(max_length=20)
     price = models.IntegerField()
     origin = models.URLField()
     description = models.TextField()
 
 
     def __str__(self):
-        return self.item
+        return self.item_name
